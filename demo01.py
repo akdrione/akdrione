39ede27778a78fe6d6bf9339218143f8bca6f4fc
akdrione <akdrione@163.com>
2020-07-29 02:36:42 -0400

    diyicixianggithhubtijiaodaima

---
+++ demo01.py
@@ -0,0 +1,40 @@
+
+# print("hello worrld!")
+# print(23333)
+# print(2.333)
+# print(True)
+# print(())
+# print([])
+# print({})
+# print('nin'+'fsdafd')
+# print('您好'+'卡缴费卡的缴费')
+# print('滚滚滚'*100)
+
+# a = input('please write')
+# print('input contant:',a)
+
+# a = 'jfkasdjfdsfsdfdf'
+# print(len(a))
+
+a = float(input('qingshuru:'))
+b = float(input('qingshuru:'))
+print('inputdeneirong:',a+b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
